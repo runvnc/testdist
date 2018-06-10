f38c501fdd4cb210416bd59e70d472ba491cb9ab
Jason Livesay <runvnc@gmail.com>
2018-06-09 17:40:20 -0700

    nice graph

--- app/windows/main.c
+++ app/windows/main.c
@@ -8,8 +8,8 @@
 
 #include "rs232/rs232.h"
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 1400
+#define WINDOW_HEIGHT 700
 
 #define NK_INCLUDE_FIXED_TYPES
 #define NK_INCLUDE_STANDARD_IO
@@ -20,17 +20,17 @@
 #include "../../nuklear.h"
 #include "nuklear_gdip.h"
 
-static LRESULT CALLBACK
+  static LRESULT CALLBACK
 WindowProc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
-    switch (msg) {
+  switch (msg) {
     case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    }
-    if (nk_gdip_handle_event(wnd, msg, wparam, lparam))
-        return 0;
-    return DefWindowProcW(wnd, msg, wparam, lparam);
+      PostQuitMessage(0);
+      return 0;
+  }
+  if (nk_gdip_handle_event(wnd, msg, wparam, lparam))
+    return 0;
+  return DefWindowProcW(wnd, msg, wparam, lparam);
 }
 
 struct nk_context *ctx;
@@ -39,32 +39,32 @@ int running = 1;
 int needs_refresh = 1;
 GdipFont* font;
 WNDCLASSW wc;
- 
+
 void setupWin32() {
-   RECT rect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
-    DWORD style = WS_OVERLAPPEDWINDOW;
-    DWORD exstyle = WS_EX_APPWINDOW;
-    /* Win32 */
-    memset(&wc, 0, sizeof(wc));
-    wc.style = CS_DBLCLKS;
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = GetModuleHandleW(0);
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.lpszClassName = L"NuklearWindowClass";
-    RegisterClassW(&wc);
+  RECT rect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+  DWORD style = WS_OVERLAPPEDWINDOW;
+  DWORD exstyle = WS_EX_APPWINDOW;
+  /* Win32 */
+  memset(&wc, 0, sizeof(wc));
+  wc.style = CS_DBLCLKS;
+  wc.lpfnWndProc = WindowProc;
+  wc.hInstance = GetModuleHandleW(0);
+  wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wc.lpszClassName = L"NuklearWindowClass";
+  RegisterClassW(&wc);
 
-    AdjustWindowRectEx(&rect, style, FALSE, exstyle);
+  AdjustWindowRectEx(&rect, style, FALSE, exstyle);
 
-    wnd = CreateWindowExW(exstyle, wc.lpszClassName, L"Distributor Tester",
-        style | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
-        rect.right - rect.left, rect.bottom - rect.top,
-        NULL, NULL, wc.hInstance, NULL);
+  wnd = CreateWindowExW(exstyle, wc.lpszClassName, L"Distributor Tester",
+      style | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
+      rect.right - rect.left, rect.bottom - rect.top,
+      NULL, NULL, wc.hInstance, NULL);
 
-    /* GUI */
-    ctx = nk_gdip_init(wnd, WINDOW_WIDTH, WINDOW_HEIGHT);
-    font = nk_gdipfont_create("Arial", 12);
-    nk_gdip_set_font(font);
+  /* GUI */
+  ctx = nk_gdip_init(wnd, WINDOW_WIDTH, WINDOW_HEIGHT);
+  font = nk_gdipfont_create("Arial", 12);
+  nk_gdip_set_font(font);
 }
 
 int com = 0;
@@ -83,7 +83,7 @@ void comTest() {
   printf("COM port open result: %d\n");
 }
 
- 
+
 char* lines[5000];
 
 int splitLines(char *str) {
@@ -98,7 +98,7 @@ int splitLines(char *str) {
 
 FILE *datafile = NULL;
 struct nk_image graphimg;
- 
+
 void checkCOM() {
   char msg[999024];
   int bytes = comRead(com, msg, 999024); 
@@ -106,31 +106,27 @@ void checkCOM() {
     int numLines = splitLines(msg);
     for (int i = 0; i < numLines; i++) {
       char* line = lines[i];
-			printf("Serial data: %s", line);
-			char* str = strtok(line, " \r\n");
-			printf("Command is xx%sxx\n", str);
+      char* str = strtok(line, " \r\n");
       if (strcmp(str, "UPLOAD")==0) {
         datafile = fopen("test.dat", "w");
         remove("img1.png");
       } else if (strcmp(str, "DATA")==0) {
-				str = strtok(NULL, " \r\n");
-				float x = atof(str);
-				str = strtok(NULL, " \r\n");
-				float y = atof(str);
-				printf("(%f, %f)\n", x, y);
-        fprintf(datafile,"%f %f\n",x,y);
-			} else if (strcmp(str,"END")==0) {
-				printf("Now we plot the chart.\n");
+        str = strtok(NULL, " \r\n");
+        float x = atof(str);
+        str = strtok(NULL, " \r\n");
+        float y = atof(str);
+        if (!(x==0 && y==0)) fprintf(datafile,"%f %f\n",x,y);
+      } else if (strcmp(str,"END")==0) {
         fclose(datafile);
         nk_gdip_image_free(graphimg);
         int ok = remove("img1.png");
         if (ok == 0) printf("Delete OK\n"); else printf("Delete failed.\n");
-        
+
         system("DEL img1.png");
         system("gnuplot\\gnuplot.exe showresults.gpi");
         graphimg = nk_gdip_load_image_from_file(L"img1.png");    
-			}
-		}
+      }
+    }
   }
 }
 
@@ -141,74 +137,65 @@ void writeCOM(const char* msg) {
 
 int main(void)
 {
-    setvbuf(stdout, (char*)NULL, _IONBF, 0);
+  setvbuf(stdout, (char*)NULL, _IONBF, 0);
 
-    setupWin32();
+  setupWin32();
 
-    comTest();
+  comTest();
 
-   graphimg = nk_gdip_load_image_from_file(L"img1.png");
+  graphimg = nk_gdip_load_image_from_file(L"img1.png");
 
-    while (running)
+  while (running)
+  {
+    checkCOM();
+
+    /* Input */
+    MSG msg;
+    nk_input_begin(ctx);
+
+    while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
+      if (msg.message == WM_QUIT)
+        running = 0;
+      TranslateMessage(&msg);
+      DispatchMessageW(&msg);
+      needs_refresh = 1;
+    }
+    nk_input_end(ctx);
+
+    /* GUI */
+    if (nk_begin(ctx, "Analysis", nk_rect(20, 20, 900, 500),
+          NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+          NK_WINDOW_CLOSABLE|NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
     {
-	checkCOM();
-    
-        /* Input */
-        MSG msg;
-        nk_input_begin(ctx);
+      nk_layout_row_static(ctx, 420, 900, 1);
+      nk_layout_row_static(ctx, 30, 80, 2);
+      if (nk_button_label(ctx, "START")) {
+        fprintf(stdout, "start pressed\n");
+        writeCOM("START\n");
+      }
+      if (nk_button_label(ctx, "STOP")) {
+        fprintf(stdout, "stop pressed\n");
+        writeCOM("STOP\n");
+      }
 
-	while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
-    	  if (msg.message == WM_QUIT)
-                running = 0;
-            TranslateMessage(&msg);
-            DispatchMessageW(&msg);
-            needs_refresh = 1;
-        }
-        nk_input_end(ctx);
+      struct nk_rect bounds = nk_window_get_bounds(ctx);
+      bounds.y = 40;
+      bounds.w = 800;
+      bounds.h = 400;
 
-        /* GUI */
-        if (nk_begin(ctx, "Analysis", nk_rect(50, 50, 200, 200),
-            NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-            NK_WINDOW_CLOSABLE|NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
-        {
-            enum {EASY, HARD};
-            static int op = EASY;
-            static int property = 20;
-            nk_layout_row_static(ctx, 500, 500, 1);
-            nk_layout_row_static(ctx, 30, 80, 2);
-            if (nk_button_label(ctx, "START")) {
-              fprintf(stdout, "start pressed\n");
-	      writeCOM("START\n");
-	    }
-            if (nk_button_label(ctx, "STOP")) {
-              fprintf(stdout, "stop pressed\n");
-	      writeCOM("STOP\n");
-	    }
- 
-            nk_layout_row_dynamic(ctx, 30, 2);
-            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
-            nk_layout_row_dynamic(ctx, 200, 1);
-	    nk_layout_row_dynamic(ctx, 150, 1);
-            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+      nk_draw_image(nk_window_get_canvas(ctx), bounds, &graphimg, nk_white);
+    }
 
-       	    struct nk_rect bounds = nk_window_get_bounds(ctx);
-	    bounds.w = 500;
-	    bounds.h = 500;
-             
-            nk_draw_image(nk_window_get_canvas(ctx), bounds, &graphimg, nk_white);
-	}
-	    
-       	nk_end(ctx);
+    nk_end(ctx);
 
 
-        /* Draw */	
-        nk_gdip_render(NK_ANTI_ALIASING_ON, nk_rgb(30,30,30));
-	
-     }
+    /* Draw */  
+    nk_gdip_render(NK_ANTI_ALIASING_ON, nk_rgb(30,30,30));
 
-    nk_gdipfont_del(font);
-    nk_gdip_shutdown();
-    UnregisterClassW(wc.lpszClassName, wc.hInstance);
-    return 0;
+  }
+
+  nk_gdipfont_del(font);
+  nk_gdip_shutdown();
+  UnregisterClassW(wc.lpszClassName, wc.hInstance);
+  return 0;
 }
--- arduino/testdist/testdist.ino
+++ arduino/testdist/testdist.ino
@@ -1,5 +1,5 @@
 String mode = "IDLE";
-const int DATAPOINTS = 600;
+const int DATAPOINTS = 10000;
 float data[DATAPOINTS][2];
 int p = 0;
 
@@ -34,11 +34,18 @@ String readLine() {
   return String(line);
 }
 
+//float shift = random(0,250);
+float shift = 100;
+float factor = 300;
+//float factor = random(230,350);
+
 void clearData() {
   for (int i=0; i<DATAPOINTS; i++) {
     data[i][0] = -1000;
     data[i][1] = -1000;
   }
+  shift = random(50,200);
+  factor = random(280,320);
 }
 
 void checkCommand() {
@@ -50,17 +57,30 @@ void checkCommand() {
     } else if (cmd == "STOP") {
       mode = "UPLOAD";
     }
-  }  
+  }
+}
+
+float lastAngle = -1000;
+
+float angleFromRPM(float rpm) {
+  float angle = 15.0*atan((rpm-shift)/factor)+1.0;
+  if (angle < 0 && rpm > 500) return lastAngle;
+  if (angle < 0) angle = 0;
+  lastAngle = angle;
+  return angle;
+  //return 15.0*atan((rpm-100.0)/300.0)+1.0;
 }
 
 void run_() {
-  blink(100);
-  data[p][0] = p;
-  data[p][1]= random(0,50);
+  digitalWrite(LED_BUILTIN, HIGH);
+  data[p][0] = p*1.0;
+  data[p][1]= angleFromRPM(p);
   p++;
-  if (p >= DATAPOINTS) {
+  if (p >= 4000) {
     mode = "UPLOAD";
   }
+  if ((p)%100==0) SerialUSB.println("STATUS "+String(p));
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void upload() {
