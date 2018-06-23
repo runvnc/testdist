ad5905f9d29c259fce1c5be8624eeef34cf5ba8d
Jason Livesay <runvnc@gmail.com>
2018-06-23 00:11:44 -0700

    handle increment and index pulse, calculate rpm and angle

--- arduino/testdist/testdist.ino
+++ arduino/testdist/testdist.ino
@@ -1,8 +1,58 @@
+float EDGES_PER_REVOLUTION = 2048;
 String mode = "IDLE";
 const int DATAPOINTS = 10000;
 float data[DATAPOINTS][2];
-int p = 0;
 
+// Arduino reference says make them volatile
+// if they are used in interrupt handlers
+volatile float angleDegrees = 0;
+volatile float initialAngle = 0;
+volatile float advanceAngle = 0;
+volatile int rotations = 0;
+volatile int startMS = 0;
+volatile int angleIncrements = 0;
+int targetRPM = 100;
+int rpmUpdateIntervalMS = 50;
+
+int p = 0; // index into data[]
+
+void start() {
+  startMS = millis();
+}
+
+void zeroAnglePulse() {
+  angleIncrements = 0;
+  rotations++;
+}
+
+void incrementAngleEdge() {
+  angleIncrements++;
+}
+
+void calculateAngle() {
+  angleDegrees = (angleIncrements / EDGES_PER_REVOLUTION) * 360;
+  if (mode == "SETUP")
+    initialAngle = angleDegrees;
+  else
+    advanceAngle = angleDegrees - initialAngle;
+}
+
+void distributorFired() {
+  calculateAngle();
+}
+
+void adjustMotor() {
+  
+}
+
+void updateRPM() {
+  int elapsedMS = millis()-startMS;
+  rotationsPerSecond = rotations / (elapsedMS/1000.0);
+  rpm = rotationsPerSecond * 60;
+  if (abs(rpm-targetRPM)>100) adjustMotor();
+  startMS = millis();
+  rotations = 0;
+}
 
 void blink(int d) {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
@@ -14,11 +64,19 @@ void blink(int d) {
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   blink(250);
+
   randomSeed(analogRead(0));
   SerialUSB.begin(115200);
   while (!Serial1) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+
+  //pinMode(indexPulsePin, INPUT);
+  //attachInterrupt(digitalPinToInterrupt(indexPulsePin),zeroAnglePulse,CHANGE);
+
+  //pinMode(incrementPin, INPUT);
+  //attachInterrupt(digitalPinToInterrupt(incrementPin),incrementAngleEdge,CHANGE);
+
   blink(250);
 }
 
@@ -71,6 +129,7 @@ float angleFromRPM(float rpm) {
   //return 15.0*atan((rpm-100.0)/300.0)+1.0;
 }
 
+/*
 void run_() {
   digitalWrite(LED_BUILTIN, HIGH);
   data[p][0] = p*1.0;
@@ -81,6 +140,32 @@ void run_() {
   }
   if ((p)%100==0) SerialUSB.println("STATUS "+String(p));
   digitalWrite(LED_BUILTIN, LOW);
+} */
+
+int lastRPMUpdate = -100;
+int startMS = -100;
+float fakeSpeed = 1.0;
+
+void run_() {
+  int ms = millis();
+  int elapsedRPM = 0;
+  if (lastRPMUpdate != -100) elapsedRPM = ms - lastRPMUpdate;
+  if (elapsedRPM >= rpmUpdateIntervalMS) {
+    updateRPM();
+    lastRPMUpdate = ms;
+  }
+  int elapsedMS = 0;
+  if (startMS != -100)
+    elapsedMS = ms - startMS;
+  if (elapsedMS > 8.0/fakeSpeed) {
+    incrementAngleEdge(); // FAKE INTERRUPT FIRE
+  }
+  if (elapsedMS > 200/fakeSpeed) {
+    zeroAnglePulse(); // FAKE INTERRUPT FIRE
+  }
+  if (elapsedMS > 275/fakeSpeed) {
+    distributorFired(); // FAKE INTERRUPT
+  }
 }
 
 void upload() {
